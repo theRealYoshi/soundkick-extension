c3f0816cb28eb2af3a25c5df72883de6fbb07c35
Yoshihiro Luk <hiroluk@gmail.com>
2016-07-03 19:40:46 -0700

    added badge

--- public/background.js
+++ public/background.js
@@ -3,60 +3,52 @@
 
 /*global chrome:false */
 
-var soundcloudOpen = false;
+chrome.browserAction.setPopup({ popup: "https://soundkick-server.herokuapp.com/"});
 
-function checkSoundcloudAccess(){
+chrome.storage.local.set({
+  soundkickAccess: false,
+  soundcloudTabActivated: false
+})
+// do a storage change listener
+
+function checkSoundkickAccess(){
+  console.log('Soundkick Access accessed');
+  // if session token in soundkick have access then check for upcoming concerts
+  // also set chrome.storage.local to true that you have access
   chrome.storage.local.set({
-    soundcloudOpen: soundcloudOpen
-  })
+    soundcloudTabActivated: true
+  });
+  // else ask user to signin to soundkick access
+
+  // chrome.browserAction.getPopup(function(res){
+  //   console.log(res);
+  // });
+  chrome.browserAction.setBadgeText({text: '1'});
   // chrome.webRequest.
 }
 
-// set to see if soundcloud is a tab that's opened
-// make this into a function os it can be called at anytime
-function checkSoundcloudOpen(){
-  chrome.tabs.query({ currentWindow: true}, function(tabs){
-    for (var tab of tabs){
-      if(tab.url.toString().indexOf("soundcloud") > -1){
-        // chrome.browserAction.setBadgeText({text: '(ツ)'});
-        // chrome.browserAction.setBadgeBackgroundColor({color: '#eae'});
-        console.log("yes");
-        console.log(tab.url);
-        soundcloudOpen = true;
-        chrome.browserAction.getPopup(function(res){
-          console.log(res);
-        });
-      }
-    }
-  })
+chrome.runtime.onMessage.addListener(dispatch);
+
+function dispatch(message){
+  console.log(message);
+  switch(message.type){
+    case "tabActivated":
+      checkSoundkickAccess();
+      console.log(message.content);
+      break;
+    case "notificationActivated":
+      notify(message.content);
+      break;
+    default:
+      console.log("nothing happened in dispatch");
+  }
 }
 
-// check to see whenever a new tab is opened. Check for soundcloud
-// or if the page requested to was soundcloud
-
-chrome.browserAction.setPopup({ popup: "https://soundkick-server.herokuapp.com/"});
-
-chrome.browserAction.onClicked.addListener(function(aTab) {
-  // set to see if authenticated
-  console.log("loading...");
-  console.log(soundcloudOpen);
-  // create popup for concert viewer
-  // If not authenticated then direct to connect page.
-  // chrome.tabs.create({'url': 'https://soundkick-server.herokuapp.com/', 'active': true});
-});
-
-chrome.cookies.getAllCookieStores(function(cookieStores){
-  console.log(cookieStores);
-})
-
-
-chrome.runtime.onMessage.addListener(notify);
-
-function notify(message) {
+function notify(messageContent) {
   chrome.notifications.create({
     "type": "basic",
     "iconUrl": chrome.extension.getURL("musical-64.png"),
     "title": "You went to Soundcloud!",
-    "message": message.content
+    "message": messageContent
   });
 }
--- public/scripts/soundcloud.js
+++ public/scripts/soundcloud.js
@@ -1,12 +1,23 @@
-console.log('this script started running');
+console.log('Soundcloud content script is running...');
 
-
-// check only first time in session, keep localstorage as false initially when browser is opened. 
+// check only first time in session, keep localstorage as false initially when browser is opened.
 window.addEventListener("focus", function(){
-  console.log(window.visibilityState);
-  notifyExtension();
+  chrome.storage.local.set({
+    soundcloudTabActivated: true
+  })
+  var firstTimeTabActivated = chrome.storage.local.get('soundcloudTabActivated');
+  if(!firstTimeTabActivated){
+    notifyExtension("tabActivated", "You're at Soundcloud!");
+  } else {
+    notifyExtension("notificationActivated", "This is your 2nd time at the Soundcloud tab!");
+  }
 });
 
-function notifyExtension() {
-  chrome.runtime.sendMessage({"content": "You're at Soundcloud!"});
+
+
+function notifyExtension(messageType, messageContent) {
+  chrome.runtime.sendMessage({
+    type: messageType,
+    content: messageContent
+  });
 }

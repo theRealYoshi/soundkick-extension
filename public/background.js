c5285dd589493d3e71138efa545afa2351aec232
Yoshihiro Luk <hiroluk@gmail.com>
2016-07-12 20:25:10 -0700

    added messaging to extension

--- public/background.js
+++ public/background.js
@@ -2,9 +2,8 @@
 'use strict';
 
 /*global chrome:false */
-
+console.log("[background.js] : setting popup view");
 chrome.browserAction.setPopup({ popup: "https://soundkick-server.herokuapp.com/"});
-
 chrome.storage.local.set({
   soundkickAccess: false,
   soundcloudTabActivated: false
@@ -12,22 +11,25 @@ chrome.storage.local.set({
 // do a storage change listener or sync
 
 function checkSoundkickAccess(){
-  console.log('Soundkick Access accessed');
+  console.log('[background.js] checkSoundkickAccess: Soundkick Access accessed');
   // if session token in soundkick have access then check for upcoming concerts
   // also set chrome.storage.local to true that you have access
   chrome.storage.local.set({
     soundcloudTabActivated: true
   });
+  getAllCookieStores();
   // else ask user to signin to soundkick access
-  chrome.browserAction.openPopup(function(popupView){
-    console.log(popupView);
-  });
+  // chrome.browserAction.openPopup(function(popupView){
+  //   console.log("[background.js] checkSoundkickAccess: " + popupView);
+  // });
+  console.log("[background.js] checkSoundkickAccess: setting badge");
   chrome.browserAction.setBadgeText({text: '1'});
   // chrome.webRequest.
 }
 
 function removeBadge(){
-  console.log("badge removed");
+  // will not fire if icon action has a popup.
+  console.log("[background.js] removeBadge: badge removed");
   chrome.browserAction.setBadgeText({text: ''});
 }
 
@@ -38,23 +40,24 @@ chrome.storage.onChanged.addListener(logStorageChange);
 // sends message to content_scripts soundcloud.js which notifies the token has been activated
 // get tab id
 
-
 function dispatch(message){
-  console.log(message);
+  console.log("[background.js] dispatch: " + message);
   switch(message.type){
     case "tabActivated":
       checkSoundkickAccess();
-      console.log(message.content);
+      console.log("[background.js] dispatch: tabActivated");
       break;
     case "notificationActivated":
+      console.log("[background.js] dispatch: notificationActivated");
       notify(message.content);
       break;
     default:
-      console.log("nothing happened in dispatch");
+      console.log("[background.js] dispatch: nothing happened in dispatch");
   }
 }
 
 function notify(messageContent) {
+  console.log("[background.js] notify: notification created");
   chrome.notifications.create({
     "type": "basic",
     "iconUrl": chrome.extension.getURL("musical-64.png"),
@@ -67,15 +70,23 @@ function notify(messageContent) {
 }
 
 function logStorageChange(changes, area) {
-  console.log("Change in storage area: " + area);
+  console.log("[background.js] logStorageChange: Change in storage area: ");
+  console.log(area);
+  console.log(changes);
+  // var changedItems = Object.keys(changes);
+  // for (item of changedItems) {
+  //   console.log("[background.js] logStorageChange: ");
+  //   console.log(item + " has changed:");
+  //   console.log("[background.js] logStorageChange: Old value: ");
+  //   console.log(changes[item].oldValue);
+  //   console.log("[background.js] logStorageChange: New value: ");
+  //   console.log(changes[item].newValue);
+  // }
+}
 
-  var changedItems = Object.keys(changes);
-  console.log(changedItems);
-  for (item of changedItems) {
-    console.log(item + " has changed:");
-    console.log("Old value: ");
-    console.log(changes[item].oldValue);
-    console.log("New value: ");
-    console.log(changes[item].newValue);
-  }
+function getAllCookieStores(){
+  chrome.cookies.getAll({url: "https://*.soundkick-server.herokuapp.com/*", name:"connect.sid"}, function(cookies){
+    console.log("[background.js] getAllCookieStores: ");
+    console.log(cookies);
+  });
 }
--- public/scripts/soundcloud.js
+++ public/scripts/soundcloud.js
@@ -3,8 +3,8 @@ console.log('Soundcloud content script is running...');
 // check only first time in session, keep localstorage as false initially when browser is opened.
 window.addEventListener("focus", function(){
   // receives message saying that this person has already activated the tab or base if off of session data.
-  var firstTimeTabActivated = true;
-  console.log(firstTimeTabActivated);
+  var firstTimeTabActivated = false;
+  console.log("[soundcloud.js] addEventListener: " + firstTimeTabActivated);
   if(!firstTimeTabActivated){
     notifyExtension("tabActivated", "You're at Soundcloud!");
   } else {
@@ -12,7 +12,9 @@ window.addEventListener("focus", function(){
   }
 });
 
+
 function notifyExtension(messageType, messageContent) {
+  console.log("[soundcloud.js] notifyExtension: " + messageContent);
   chrome.runtime.sendMessage({
     type: messageType,
     content: messageContent
@@ -20,13 +22,12 @@ function notifyExtension(messageType, messageContent) {
 }
 
 function receiveMessage(request, sender, sendResponse){
-  console.log("this was received from content scripts");
+  console.log("[soundcloud.js] receiveMessage: " + request);
   console.log(request);
   console.log(sender);
   console.log(sendResponse);
-  console.log("this is the message content");
-  console.log(request.content);
+  console.log("[soundcloud.js] receiveMessage: this is the message content");
+  console.log("[soundcloud.js] receiveMessage: " + request.content);
 }
-
 
 chrome.runtime.onMessage.addListener(receiveMessage);
